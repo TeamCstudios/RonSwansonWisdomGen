8593d997a39008e4475b72d64a13dac53a59df39
Chew <Chewsterchew@users.noreply.github.com>
2017-07-25 19:01:34 -0500

    allow another blank

--- changemessage.js
+++ changemessage.js
@@ -1,5 +1,5 @@
 function changeMessage(){
-    for(var i=0; i<4; i++){
+    for(var i=0; i<5; i++){
             document.getElementById(i+"line").innerHTML=lines2[i][(Math.floor(Math.random()*lines2[i].length))];
     }
 }
