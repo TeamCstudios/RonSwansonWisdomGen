65aa8564b43c4421c7f06e150fca8edf4a3d0880
Chew <Chewsterchew@users.noreply.github.com>
2017-07-26 15:31:45 -0500

    randomizer

--- changemessage.js
+++ changemessage.js
@@ -1,6 +1,13 @@
 function changeMessage(){
-    var x = Randomizer.nextInt(0,1);
+    var phrasetype = Randomizer.nextInt(0,1);
+    if (phrasetype == 0) {
     for(var i=0; i<5; i++){
             document.getElementById(i+"line").innerHTML=lines2[i][(Math.floor(Math.random()*lines2[i].length))];
+    }
+    }
+    else {
+        for(var i=0; i<5; i++){
+            document.getElementById(i+"line").innerHTML=lines3[i][(Math.floor(Math.random()*lines3[i].length))];
+    }
     }
 }
