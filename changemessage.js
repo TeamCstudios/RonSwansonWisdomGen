206a19033489d20900c6598fa06fe8cea0deb1ac
MrJoCrafter <cstudios35511@gmail.com>
2017-07-26 15:30:27 -0500

    Update changemessage.js

--- changemessage.js
+++ changemessage.js
@@ -1,4 +1,5 @@
 function changeMessage(){
+    var x = Randomizer.nextInt(0,1);
     for(var i=0; i<5; i++){
             document.getElementById(i+"line").innerHTML=lines2[i][(Math.floor(Math.random()*lines2[i].length))];
     }
