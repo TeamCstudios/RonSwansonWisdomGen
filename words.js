b6357a4f1e7d3a4a8858bc4ddbce50fb412ecf65
Chewsterchew <chew@chew.pw>
2017-09-16 18:16:28 -0500

    easier to add words, also new purple

--- words.js
+++ words.js
@@ -1,77 +1,32 @@
+var phrase1word1 = "Minecraft,Terraria,Political Correctness,Bagels,Dank Memes,The Internet,GitHub,YouTube,FNAF,Undertale,The word \" welp \",Birthdays,Calendars,Cake"
+
+var phrase1word2 = "Facebook,Amazon,Team Cstudios,the mainstream media,the Illuminati,BobTheBagel,Twitter,Chew,MrJoCrafter"
+
+var phrase1word3 = "toys,merchandise,cards,memes,children,games,more food,useless junk,instruments,tools,books,albums"
+
+var phrase2word1 = "Existing,Crying,Talking,Not dying,Memeing,Worshipping Satan,Researching,Standardizing,Individualizing,Laughing,Making out,Loving"
+
+var phrase2word2 = "funerals,the Grand Canyon,birthday parties,discussion boards,4chan,reddit,YouTube comment section,anniversary parties,meme parties,Bob's funeral,Making out,loving"
+
+
+var p1w1array = phrase1word1.split(",");
+
+var p1w2array = phrase1word2.split(",");
+
+var p1w3array = phrase1word3.split(",");
+
+var p2w1array = phrase2word1.split(",");
+
+var p2w2array = phrase2word2.split(",");
+
+
 var lines2 = [
-
-    ["Minecraft",
-"Terraria",
-"Political Correctness",
-"Bagels",
-"Dank Memes",
-"The Internet",
-"GitHub",
-"YouTube",
-"FNAF",
-"Undertale",
-"The word \"welp\"",
-"Birthdays",
-"Calendars",
-"Cake"],
-
-    ["was created by"],
-
-    ["Facebook",
-"Amazon",
-"Team CStudios",
-"the mainstream media",
-"the Illuminati",
-"Bob The Bagel",
-"Twitter",
-"Chew",
-"MrJoCrafter"],
-
-    ["to sell"],
-
-    ["toys",
-"merchandise",
-"cards",
-"memes",
-"children",
-"games",
-"more food",
-"useless junk",
-"instruments",
-"tools",
-"books",
-"albums"],
+  p1w1array, ["was created by"],
+  p1w2array, ["to sell"],
+  p1w3array,
 ];
 
 var lines3 = [
-
-["Existing",
-"Crying",
-"Talking",
-"Not dying",
-"Meming",
-"Worshipping Satan",
-"Researching",
-"Standardizing",
-"Individualizing",
-"Laughing",
-"Making out",
-"Loving"],
-
-    ["is only acceptable at"],
-
-    ["funerals",
-"the Grand Canyon",
-"birthday parties",
-"discussion boards",
-"4chan",
-"reddit",
-"YouTube comment sections",
-"anniversary parties",
-"meme parties",
-"Bob's funeral",
-"Making out",
-"loving"],
-
-    ["and"]
+  p2w1array, ["is only acceptable at"],
+  p2w2array, ["and"]
 ];
