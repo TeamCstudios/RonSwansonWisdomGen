f22f654a0d89798cea18f594886747ca4e92d0b2
MrJoCrafter <cstudios35511@gmail.com>
2017-10-20 13:52:09 -0500

    Update words.js

--- words.js
+++ words.js
@@ -1,32 +1,23 @@
+//Word Lists
 var phrase1word1 = "Minecraft,Terraria,Political Correctness,Bagels,Dank Memes,The Internet,GitHub,YouTube,FNAF,Undertale,The word \" welp \",Birthdays,Calendars,Cake"
-
 var phrase1word2 = "Facebook,Amazon,Team Cstudios,the mainstream media,the Illuminati,BobTheBagel,Twitter,Chew,MrJoCrafter"
-
 var phrase1word3 = "toys,merchandise,cards,memes,children,games,more food,useless junk,instruments,tools,books,albums"
-
 var phrase2word1 = "Existing,Crying,Talking,Not dying,Memeing,Worshipping Satan,Researching,Standardizing,Individualizing,Laughing,Making out,Loving"
-
 var phrase2word2 = "funerals,the Grand Canyon,birthday parties,discussion boards,4chan,reddit,YouTube comment section,anniversary parties,meme parties,Bob's funeral,Making out,loving"
 
-
+//Split Arrays
 var p1w1array = phrase1word1.split(",");
-
 var p1w2array = phrase1word2.split(",");
-
 var p1w3array = phrase1word3.split(",");
-
 var p2w1array = phrase2word1.split(",");
-
 var p2w2array = phrase2word2.split(",");
 
 
+
+//Functions
 var lines2 = [
-  p1w1array, ["was created by"],
-  p1w2array, ["to sell"],
-  p1w3array,
+  p1w1array, ["was created by"], p1w2array, ["to sell"], p1w3array,];
+var lines3 = [
+  p2w1array, ["is only acceptable at"], p2w2array, ["and"]
 ];
 
-var lines3 = [
-  p2w1array, ["is only acceptable at"],
-  p2w2array, ["and"]
-];
