a0553a87095655c817f23c1006b0a0a9e07b39bb
MrJoCrafter <cstudios35511@gmail.com>
2018-11-29 11:09:24 -0600

    Add phrase 3 (hopefully)

--- changemessage.js
+++ changemessage.js
@@ -5,10 +5,16 @@ function changeMessage(){
             document.getElementById(i+"line").innerHTML=lines2[i][(Math.floor(Math.random()*lines2[i].length))];
      }
     }
-    else {
+    else if(phrasetype == 1){
         for(var i=0; i<4; i++){
             document.getElementById(i+"line").innerHTML=lines3[i][(Math.floor(Math.random()*lines3[i].length))]
             document.getElementById("4line").innerHTML=lines3[2][(Math.floor(Math.random()*lines3[2].length))];
+        }
+    }
+    else{
+        for(var i=0; i<5; i++){
+            document.getElementById(i+"line").innerHTML=lines4[i][(Math.floor(Math.random()*lines4[i].length))]
+            document.getElementById("4line").innerHTML=lines4[2][(Math.floor(Math.random()*lines4[2].length))];
         }
     }
     document.getElementById("startBttn").textContent = "Again?";
