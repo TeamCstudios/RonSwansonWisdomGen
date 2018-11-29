a4bb5401db3a0d43b6f801f7c5890925050f9407
MrJoCrafter <cstudios35511@gmail.com>
2018-11-29 11:04:12 -0600

    Adding phrase 3

--- words.js
+++ words.js
@@ -4,6 +4,8 @@ var phrase1word2 = "Facebook,Amazon,Team Cstudios,the mainstream media,the Illum
 var phrase1word3 = "toys,merchandise,cards,memes,children,games,more food,useless junk,instruments,tools,books,albums"
 var phrase2word1 = "Existing,Crying,Talking,Not dying,Memeing,Worshipping Satan,Researching,Standardizing,Individualizing,Laughing,Making out,Loving"
 var phrase2word2 = "funerals,the Grand Canyon,birthday parties,discussion boards,4chan,reddit,YouTube comment section,anniversary parties,meme parties,Bob's funeral,Making out,loving"
+var phrase3word1 = "memes,cool things,greatness,the world and stuff"
+var phrase3word2 = "meat,memes,video games,the letter Q,libertarianism,America,Rick Astley,romance,alcohol"
 
 //Split Arrays
 var p1w1array = phrase1word1.split(",");
@@ -11,13 +13,15 @@ var p1w2array = phrase1word2.split(",");
 var p1w3array = phrase1word3.split(",");
 var p2w1array = phrase2word1.split(",");
 var p2w2array = phrase2word2.split(",");
-
+var p3w1array = phrase3word1.split(",");
+var p3w2array = phrase3word2.split(",");
 
 
 //Functions
 var lines2 = [
   p1w1array, ["was created by"], p1w2array, ["to sell"], p1w3array,];
 var lines3 = [
-  p2w1array, ["is only acceptable at"], p2w2array, ["and"]
-];
+  p2w1array, ["is only acceptable at"], p2w2array, ["and"]];
+var lines4 = [
+  ["This is the Swanson pyramid of"], p3w1array, [". At the top,"], p3w2array, [","], p3w2array, ["and"], p3w2array,];
 
